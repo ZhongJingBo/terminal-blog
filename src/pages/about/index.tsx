3fbea13476c43c9cb2035eeaefbeeefaf0f3c2ba
钟靖博 <zhongjingbo@zhongjingbos-MacBook-Pro.local>
2024-04-26 14:24:40 +0800

    update

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -12,11 +12,10 @@ const About: FC = () => {
         <a href="https://yucihent.space/" target="_blank">
           点击前往作者地址
         </a>
-        ,对于样式进行了像素级的复刻。
       </div>
       <div>整体使用的技术有所不同，作者使用的是Next作为技术框架，想要有更多了解可以进入原作者的网站了解更多。</div>
-    
-      <div>关于terminal-blog</div>
+      <br/>
+      <div>关于技术</div>
       <div>
          框架使用的是React18，构建是采取Rollup，想要知道更多关于博客的技术细节可以
         <a>点击这里</a>
